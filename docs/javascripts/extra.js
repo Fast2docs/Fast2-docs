0dbf7083ed91e254df9cf97ecaa865f1260fa461
Corentin LEBAS <corentinlebas45@gmail.com>
2023-11-20 18:47:17 +0100

    [ADD] Pop-up

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -38,3 +38,49 @@ document$.subscribe(function () {
     console.log("clean localstorage");
   }
 });
+
+
+function generatePopup() {
+  var popupContainer = document.createElement("div");
+  popupContainer.className = "popup-container hidden";
+  popupContainer.innerHTML = `
+    <div class="popup-icon">
+      <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+      </svg>
+    </div>
+    <div class="popup-content">
+      <p class="text-sm text-gray-700 dark:text-gray-400">
+        You don't find what you want ? <br>
+        Did you check the <a href="https://arondor.atlassian.net/jira/servicedesk/projects/TMAFAST/knowledge/articles">Knowledge Base</a> ?
+      </p>
+      <button class="dont-show-again-btn">Don't show again</button>
+    </div>
+  `;
+  document.body.appendChild(popupContainer);
+
+  var dontShowAgainBtn = popupContainer.querySelector(".dont-show-again-btn");
+  dontShowAgainBtn.addEventListener("click", function () {
+    sessionStorage.setItem("popupHidden", "true");
+    popupContainer.classList.add("hidden");
+  });
+}
+
+function verifierPositionDeDefilement() {
+  if (!sessionStorage.getItem("popupHidden") && window.scrollY >= document.documentElement.scrollHeight / 2) {
+    var popupContainer = document.querySelector(".popup-container");
+    if (popupContainer.classList.contains("hidden")) {
+      popupContainer.classList.remove("hidden");
+      setTimeout(function () {
+        popupContainer.classList.add("hidden");
+      }, 5000);
+    }
+  }
+}
+
+window.addEventListener("scroll", verifierPositionDeDefilement);
+
+window.addEventListener("beforeunload", function () {
+  sessionStorage.removeItem("popupHidden");
+});
+generatePopup();
